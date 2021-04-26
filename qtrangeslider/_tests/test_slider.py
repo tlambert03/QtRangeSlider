52e10ad52364392c7cd5940e0e09ea819b8e22a5
Talley Lambert <talley.lambert@gmail.com>
2021-04-26 08:12:14 -0400

    add mouse drag

--- qtrangeslider/_tests/test_slider.py
+++ qtrangeslider/_tests/test_slider.py
@@ -8,3 +8,37 @@ from qtrangeslider.qtcompat.QtCore import Qt
 def test_basic(qtbot, orientation):
     rs = QRangeSlider(getattr(Qt, orientation))
     qtbot.addWidget(rs)
+
+
+def test_drag(qtbot, qapp):
+    rs = QRangeSlider(Qt.Horizontal)
+    rs.setMouseTracking(True)
+    qtbot.addWidget(rs)
+    rs.show()
+    rs.sliderMoved.connect(lambda e: print("moved", e))
+
+    opt = rs._getStyleOption()
+    pos = rs._handleRects(opt, 0).center()
+    qtbot.mousePress(rs, Qt.LeftButton, pos=pos)
+    assert rs._pressedControl == ("handle", 0)
+
+    for _ in range(15):
+        pos.setX(pos.x() + 2)
+        qtbot.mouseMove(rs.window(), pos)
+    qtbot.mouseRelease(rs, Qt.LeftButton)
+
+    assert rs.value() == (35, 80)
+    assert rs._pressedControl == rs._NULL_CTRL
+
+    opt = rs._getStyleOption()
+    pos = rs._handleRects(opt, 1).center()
+    qtbot.mousePress(rs, Qt.LeftButton, pos=pos)
+    assert rs._pressedControl == ("handle", 1)
+
+    for _ in range(15):
+        pos.setX(pos.x() - 2)
+        qtbot.mouseMove(rs.window(), pos)
+    qtbot.mouseRelease(rs, Qt.LeftButton)
+
+    assert rs.value() == (35, 65)
+    assert rs._pressedControl == rs._NULL_CTRL
